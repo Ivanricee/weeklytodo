875be05741683e73a3a7e32a443441528778df4e
Iván Bautista <ivanbautistag@gmail.com>
2025-01-22 16:38:55 -0600

    feat: component to show user and todo list functions

---
+++ src/pages/TodoList.tsx
@@ -0,0 +1,48 @@
+import AddTask from '@/components/AddTask'
+import TaskList from '@/components/TaskList'
+import { Button } from '@/components/ui/button'
+import { Card, CardHeader } from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
+import useAuth from '@/hook/useAuth'
+import useTaskActions from '@/hook/useTaskActions'
+import { useOptimistic } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
+export default function TodoList() {
+  const { logOut, authState, user } = useAuth({ redirecTo: '/' })
+
+  console.log(' estado ', { authState, user })
+  if (!user) {
+    return (
+      <main className="container mx-auto bg-zinc-900/20 flex flex-col gap-4 items-center justify-center w-screen h-screen">
+        <h1 className="text-2xl">Usuario no encontrado</h1>
+        <Button asChild>
+          <Link to="/">Ir a la página de inicio</Link>
+        </Button>
+      </main>
+    )
+  }
+
+  //get data from firebase
+  return (
+    <main className="container mx-auto bg-zinc-900/20 flex flex-col w-screen h-screen max-w-[400px]">
+      <section className="flex justify-between w-full items-center py-4 h-[70px]">
+        <div>
+          <Button variant={'outline'} onClick={logOut} disabled={authState.isLoading}>
+            Log out
+          </Button>
+          <p className="text-red-400 text-sm">{authState.error && <p>{authState.error} </p>}</p>
+        </div>
+        <p className="text-muted-foreground text-sm">Hola {user.email}</p>
+      </section>
+      <Card className="flex-1 flex flex-col overflow-hidden p-4">
+        <CardHeader className="text-xl">Mi lista de tareas</CardHeader>
+
+        <TaskList />
+      </Card>
+      <section className=" py-2">
+        <h2>Footer</h2>
+      </section>
+    </main>
+  )
+}
