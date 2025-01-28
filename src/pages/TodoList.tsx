cf75444c31745eab59645b9ad59a4c72b49a7cce
Iván Bautista <ivanbautistag@gmail.com>
2025-01-28 00:30:59 -0600

    refactor: remove redirectTo prop, handle redirect in custom hook

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,11 +1,11 @@
 import TaskList from '@/components/TaskList'
 import { Button } from '@/components/ui/button'
 import { Card, CardHeader } from '@/components/ui/card'
-import useAuth from '@/hook/useAuth'
+import { useAuthContext } from '@/context/AuthContext'
 import { Link } from 'react-router-dom'
 
 export default function TodoList() {
-  const { logOut, authState, user } = useAuth({ redirecTo: '/' })
+  const { logOut, authState, user } = useAuthContext()
   if (!user) {
     return (
       <main className="container mx-auto bg-zinc-900/20 flex flex-col gap-4 items-center justify-center w-screen h-screen">
@@ -18,7 +18,7 @@ export default function TodoList() {
   }
 
   return (
-    <main className="container mx-auto bg-zinc-900/20 flex flex-col w-screen h-screen max-w-[400px]">
+    <main className="container mx-auto bg-zinc-900/20 flex flex-col w-screen h-screen max-w-[400px] animate-in fade-in-20 slide-in-from-top-5 ">
       <section className="flex justify-between w-full items-center py-4 h-[70px]">
         <div>
           <Button variant={'outline'} onClick={logOut} disabled={authState.isLoading}>
@@ -28,7 +28,7 @@ export default function TodoList() {
         </div>
         <p className="text-muted-foreground text-sm">Hola {user.email}</p>
       </section>
-      <Card className="flex-1 flex flex-col overflow-hidden p-4">
+      <Card className="flex-1 flex flex-col overflow-hidden p-4 rounded-[4rem]">
         <CardHeader className="text-xl">Mi lista de tareas</CardHeader>
         <TaskList />
       </Card>
