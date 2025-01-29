872f9dcb2e21579697f75c88b733e2e88b6f677b
Iván Bautista <ivanbautistag@gmail.com>
2025-01-29 00:00:12 -0600

    refactor: update writeTask to accept "day" prop and revert state on Firebase error

--- src/hook/useTaskActions.ts
+++ src/hook/useTaskActions.ts
@@ -1,10 +1,10 @@
-import { generateTaskId, readTaskFB, type Task, writeTaskFB } from '@/lib/firebase'
+import { generateTaskId, readTaskFB, writeTaskFB } from '@/lib/firebase'
 import { initialWeekDays, type TaskByWeek } from '@/lib/utils'
 import { addTaskSchema } from '@/schemas/addtask-schema'
 import { useEffect, useState } from 'react'
 import { z } from 'zod'
 
-type actionState = {
+export type actionState = {
   loading: boolean
   error: string | null
 }
@@ -20,26 +20,25 @@ export default function useTaskActions() {
     readTaskFB((taskByWeek) => setWeekTasks(taskByWeek))
   }, [])
 
-  const getDay = (date: number) => new Date(date).getDay()
-
-  const writeTask = async (data: z.infer<typeof addTaskSchema>) => {
-    setActionState((cState) => ({ ...cState, loading: true }))
-
+  const writeTask = async (data: z.infer<typeof addTaskSchema>, day: number) => {
+    setActionState({ error: null, loading: true })
     const title = data.title
     const draftTaskId = generateTaskId()
     if (draftTaskId) {
       const completed = false
       const date = Date.now()
-      const day = getDay(date)
       const copyWeekTasks = structuredClone(weekTasks) as TaskByWeek
-      const newTask = { taskId: draftTaskId, title, date, completed, isLoading: true }
+      const newTask = { taskId: draftTaskId, title, date, day, completed, isLoading: true }
       copyWeekTasks[day].push(newTask)
       setWeekTasks(copyWeekTasks)
       const { error } = await writeTaskFB(newTask)
-      if (error) return setActionState({ error: error, loading: false })
+      if (error) {
+        setWeekTasks(structuredClone(weekTasks))
+        return setActionState({ error: error, loading: false })
+      }
       return setActionState((cState) => ({ ...cState, loading: false }))
     }
   }
 
-  return { weekTasks, writeTask, actionState, getDay /*deleteTask*/ }
+  return { weekTasks, writeTask, actionState /*deleteTask*/ }
 }
