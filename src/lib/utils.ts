3b7c8a95cbdef8520eb0c830fbf3f70e0f5eeca1
Iván Bautista <ivanbautistag@gmail.com>
2025-02-03 18:20:04 -0600

    feat: add getCurrentWeekText, getLongDay, and getLongMonthDay utilities

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,15 +23,31 @@ export function getTaskByWeek(tasksFB: TasksObject): TaskByWeek {
       isLoading: false,
       ...(task as Omit<Task, 'taskId'>),
     }
-    console.log({ draftTask, day })
-
     //console.log({ day, draftTask })
     weekDays[day].push(draftTask)
   }
 
   return weekDays
 }
+export const getLongDay = (day: number) => {
+  const date = new Date()
+  const lang = navigator.language
+  date.setDate(date.getDate() - date.getDay() + day)
+  return date.toLocaleDateString(lang, { weekday: 'long' })
+}
+export const getLongMonthDay = (day: number) => {
+  const lang = navigator.language
+  const { weekStart } = getWeekRange()
 
+  const date = new Date(weekStart)
+  date.setDate(date.getDate() + day)
+
+  const month = date.toLocaleString(lang, { month: 'long' })
+  const dayMonth = date.getDate().toString().padStart(2, '0')
+  //const time = date.toLocaleString(lang, { hour: '2-digit', minute: '2-digit', hour12: true })
+
+  return `${month} ${dayMonth} `
+}
 export function getWeekRange() {
   const currentDate = new Date()
   const currentDay = currentDate.getDay() // Día de la semana (0 = Domingo, 1 = Lunes, etc.)
@@ -42,4 +58,21 @@ export function getWeekRange() {
   weekEnd.setDate(weekStart.getDate() + 6) // Domingo de la semana actual
   weekEnd.setHours(23, 59, 59, 999)
   return { weekStart: weekStart.getTime(), weekEnd: weekEnd.getTime() }
+}
+export function getCurrentWeekText() {
+  const { weekStart, weekEnd } = getWeekRange()
+  const startDate = new Date(weekStart)
+  const endDate = new Date(weekEnd)
+
+  const shortMonth = new Intl.DateTimeFormat(navigator.language, { month: 'short' })
+  // Obtiene el día y el mes abreviado para el inicio y fin de la semana
+  const startDay = startDate.getDate()
+  const startMonth = shortMonth.format(startDate)
+  const endDay = endDate.getDate()
+  const endMonth = shortMonth.format(endDate)
+  const year = startDate.getFullYear()
+
+  // Si la semana está en el mismo mes
+  if (startMonth === endMonth) return `${startDay} - ${endDay} ${startMonth} ${year}`
+  return `${startDay} ${startMonth} - ${endDay} ${endMonth} ${year}`
 }
