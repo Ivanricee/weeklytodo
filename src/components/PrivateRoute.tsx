a0cd6d44acca8027dcbf3f23c55ab23e592f4bc5
Iván Bautista <ivanbautistag@gmail.com>
2025-02-03 18:26:26 -0600

    refactor: update item display and padding styles

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -12,13 +12,13 @@ export default function PrivateRoute({ children }: { children: JSX.Element }) {
   }, [authState.isLoading])
   if (authState.isLoading || (isValidatingUser && !authState.isAuthenticated)) {
     return (
-      <main className="container mx-auto bg-zinc-900/20 flex flex-col w-screen h-screen max-w-[400px] animate-out fade-out-20 slide-out-from-top-5 ">
-        <section className="flex justify-between w-full items-center py-4 h-[70px]">
+      <main className="container mx-auto flex flex-col gap-4 w-screen h-screen max-w-[400px] animate-in fade-in-20 slide-in-from-top-6 ">
+        <section className="flex justify-between items-center w-full py-4 h-24">
           <Skeleton className="h-8 w-[80px] rounded-sm" />
           <Skeleton className="h-4 w-[150px] rounded-sm" />
         </section>
-        <Skeleton className="rounded-2xl flex-1 flex flex-col overflow-hidden w-full" />
-        <section className=" py-2">
+        <Skeleton className="w-full flex-1 flex flex-col overflow-hidden rounded-[3rem] p-0" />
+        <section className="flex justify-between items-center w-full py-4 h-24">
           <Skeleton className="h-10 rounded-sm  w-full" />
         </section>
       </main>
