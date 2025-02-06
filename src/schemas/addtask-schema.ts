a556965b294b0ca0adffefce8f09cf0757946bb4
Iván Bautista <ivanbautistag@gmail.com>
2025-02-06 13:51:45 -0600

    refactor: update input error message

--- src/schemas/addtask-schema.ts
+++ src/schemas/addtask-schema.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod'
 
 export const addTaskSchema = z.object({
-  title: z.string().min(3).max(300).nonempty(),
+  title: z.string().min(3, 'At least 3 chars').max(300, 'Max 300 chars').nonempty("Can't be empty"),
 })
