2d7a7e702c8935b6522dcd07a0054ff84f481ac1
Iván Bautista <ivanbautistag@gmail.com>
2025-02-10 13:41:14 -0600

    refactor: adjust background color opacity

--- src/components/WeekTasksList/SkeletonTasks.tsx
+++ src/components/WeekTasksList/SkeletonTasks.tsx
@@ -4,7 +4,7 @@ export default function SkeletonTasks() {
   return (
     <div className="flex flex-col gap-2">
       {Array.from({ length: 3 }).map((_, i) => (
-        <Skeleton className="w-full h-4 bg-secondary" key={i} />
+        <Skeleton className="w-full h-4 bg-secondary/30" key={i} />
       ))}
     </div>
   )
