68bd93c44ebb219d2329fcfaf05817aa04e4df68
Iván Bautista <ivanbautistag@gmail.com>
2025-02-10 13:40:10 -0600

    feat: add styles for task items

--- src/components/WeekTasksList/Task.tsx
+++ src/components/WeekTasksList/Task.tsx
@@ -2,9 +2,15 @@ import { type Task } from '@/lib/firebase'
 import { Checkbox } from '@/ui/checkbox'
 
 export default function Task({ task }: { task: Task }) {
+  const toggleTask = () => {
+    console.log('crear funcion para editar la task')
+  }
   return (
-    <div className="animate-in fade-in-0 slide-in-from-top-3/4">
-      <Checkbox />
+    <div
+      className="animate-in fade-in-0 slide-in-from-top-2 bg-emerald-500/5 flex flex-nowrap
+    items-center gap-2 line-through decoration-primary"
+    >
+      <Checkbox checked={task.completed} onCheckedChange={toggleTask} />
       {task.title} {task.isLoading}
     </div>
   )
