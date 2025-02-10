95d960af3b387e8e55f37ef9b1e005cba3cad519
Iván Bautista <ivanbautistag@gmail.com>
2025-02-10 13:54:37 -0600

    feat: reset input on submit and show button only when input has text

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -5,6 +5,8 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { Form } from '@/ui/form'
 import CustomFormField from './CustomFormField'
+import { SendHorizonalIcon } from 'lucide-react'
+import { Button } from '@/ui/button'
 
 interface AddTaskProps {
   writeTask: (data: z.infer<typeof addTaskSchema>, day: number) => void
@@ -18,17 +20,29 @@ export default function AddTask({ writeTask, day, areTasksEmpty }: AddTaskProps)
     },
     resolver: zodResolver(addTaskSchema),
   })
+  const titleValue = form.watch('title')
   return (
-    <div className={` overflow-hidden w-full backdrop-blur-2xl`}>
+    <div className={` overflow-hidden w-full backdrop-blur-2xl `}>
       <Form {...form}>
-        <form className="flex w-full" onSubmit={form.handleSubmit((data) => writeTask(data, day))}>
+        <form
+          className="flex w-full"
+          onSubmit={form.handleSubmit((data) => {
+            writeTask(data, day)
+            form.reset()
+          })}
+        >
           <CustomFormField
             control={form.control}
             name="title"
             placeholder="Add a new task..."
-            inputClass="p-0 px-1 border-none w-full rounded-none border-0 border-secondary/30 shadow-none focus-visible:ring-0 focus-visible:border-solid focus-visible:border-b"
+            inputClass="p-0 focus-visible:ring-0 px-1 border-none w-full rounded-sm border-0 focus-visible:bg-input/5"
             inputFocus={true}
           />
+          {titleValue.length >= 3 && (
+            <Button size="icon" type="submit">
+              <SendHorizonalIcon />
+            </Button>
+          )}
         </form>
       </Form>
     </div>
