19bcb7f56fe403c0570e5f5c7aac814cb858c2fc
Iván Bautista <ivanbautistag@gmail.com>
2025-02-10 13:44:24 -0600

    refactor: adjust accordion title padding and unwrap date from taskContent

--- src/components/WeekTasksList/index.tsx
+++ src/components/WeekTasksList/index.tsx
@@ -20,7 +20,6 @@ const currentDay = String(new Date().getDay())
 
 export default function WeekTasksList() {
   const { weekTasks, writeTask, actionState } = useTaskActions()
-
   return (
     <div className=" flex flex-col flex-1 overflow-hidden border-none">
       <ScrollArea className="border-none w-full flex-1 rounded-lg border">
@@ -36,27 +35,25 @@ export default function WeekTasksList() {
                   <AccordionItem
                     key={idx}
                     value={String(normalizedDay)}
-                    className={`${bgDays[idx]} border-none text-foreground/75 font-rubik pl-10 py-[1.692rem]  `}
+                    className={`${bgDays[idx]} border-none text-foreground/75 font-rubik pl-10 py-[0.9rem]`}
                   >
-                    <AccordionTrigger className="text-3xl font-bold uppercase scale-y-[1.1] ">
+                    <AccordionTrigger className="text-3xl font-bold uppercase scale-y-[1.1] py-[1.041rem] ">
                       {getLongDay(normalizedDay)}
                     </AccordionTrigger>
-                    <AccordionContent className="pr-4 animate-in fade-in-0 slide-in-from-top-3/4 h-52 relative ">
-                      <TaskContent actionState={actionState} key={idx}>
-                        <div className="h-full flex flex-col gap-1 w-full">
-                          {!areTasksEmpty && (
-                            <p className="text-sm text-foreground/70 font-light pb-4">
-                              {getLongMonthDay(idx)}
-                            </p>
-                          )}
+                    <AccordionContent className="pr-4 animate-in fade-in-0  h-52 relative">
+                      <div className="h-full flex flex-col gap-1 w-full">
+                        <p className="text-sm text-foreground/70 font-light pb-3 ">
+                          {getLongMonthDay(idx)}
+                        </p>
+                        <TaskContent actionState={actionState} key={idx}>
                           <TasksList tasks={normalizedTask} />
                           <AddTask
                             writeTask={writeTask}
                             day={normalizedDay}
                             areTasksEmpty={areTasksEmpty}
                           />
-                        </div>
-                      </TaskContent>
+                        </TaskContent>
+                      </div>
                     </AccordionContent>
                   </AccordionItem>
                 )
