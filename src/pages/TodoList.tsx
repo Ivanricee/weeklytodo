af2cb58049e0d1f911a7737d3eb3bd726087f4f6
Iván Bautista <ivanbautistag@gmail.com>
2025-02-17 17:12:55 -0600

    refactor: update semantic elements and add global task state to WeekTaskList

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -3,6 +3,7 @@ import WeekTasksList from '@/components/WeekTasksList'
 import { Button } from '@/components/ui/button'
 import { Card, CardHeader } from '@/components/ui/card'
 import { useAuthContext } from '@/context/AuthContext'
+import { TaskProvider } from '@/context/TaskContext'
 import { getCurrentWeekText } from '@/lib/utils'
 import { Link } from 'react-router-dom'
 
@@ -22,25 +23,28 @@ export default function TodoList() {
 
   return (
     <main className="container mx-auto flex flex-col gap-4 w-screen h-screen max-w-[400px] animate-in fade-in-20 slide-in-from-top-4 px-7">
-      <section className="flex gap-2 justify-between w-full items-center py-4 h-24">
+      <header className="flex gap-2 justify-between w-full items-center py-4 h-24">
         <div>
           <Button variant={'outline'} onClick={logOut} disabled={authState.isLoading}>
             Log out
           </Button>
-          <p className="text-red-300 text-sm">{authState.error && <p>{authState.error} </p>}</p>
+          <p className="text-red-300 text-sm">{authState.error && authState.error}</p>
         </div>
         <p className="text-muted-foreground text-sm">Hola {user.email}</p>
-      </section>
+      </header>
+
       <Card className="flex-1 flex flex-col overflow-hidden rounded-[3rem] border-[1.7px] border-secondary/50 bg-transparent shadow-3xl p-0 backdrop-blur-sm relative min-h-[33.3rem] max-h-[45.97rem]">
         <CardHeader className="pt-4 py-3 pl-10 text-base bg-transparent backdrop-blur-sm absolute left-0 right-0 z-10 flex flex-row items-baseline justify-between">
           <CurrentHour />
           <small className="text-foreground/70 font-medium">{getCurrentWeekText()}</small>
         </CardHeader>
-        <WeekTasksList />
+        <TaskProvider>
+          <WeekTasksList />
+        </TaskProvider>
       </Card>
-      <section className="flex gap-2 justify-between w-full items-center py-4 h-24">
+      <footer className="flex gap-2 justify-between w-full items-center py-4 h-24">
         <h2>Footer</h2>
-      </section>
+      </footer>
     </main>
   )
 }
