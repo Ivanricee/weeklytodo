f116a5bdcdd2f5d7570d11bb2fe6e00d2f097544
Iván Bautista <ivanbautistag@gmail.com>
2025-02-17 17:20:38 -0600

    refactor: use <ul> for wrapping task list

--- src/components/WeekTasksList/TaskList.tsx
+++ src/components/WeekTasksList/TaskList.tsx
@@ -4,14 +4,13 @@ import { ListTodo } from 'lucide-react'
 import { ScrollArea } from '@/ui/scroll-area'
 
 export default function TaskList({ tasks }: { tasks: TaskType[] }) {
-  //console.log('k es esto tio ', tasks)
-
   return (
     <ScrollArea className="flex-1 ">
-      {tasks.map((task, idx) => {
-        //console.log('como aparecen las tas? ', task)
-        return <Task key={idx} task={task} />
-      })}
+      <ul>
+        {tasks.map((task, idx) => {
+          return <Task key={idx} task={task} />
+        })}
+      </ul>
       {tasks.length === 0 && (
         <div className="animate-in fade-in-0 flex flex-col items-center justify-center h-full gap-2">
           <div className="bg-secondary/10 rounded-md aspect-square w-14 flex items-center justify-center">
