cf1e55a1fc30403099a8c22064226819e77e045f
Iván Bautista <ivanbautistag@gmail.com>
2025-02-17 17:13:46 -0600

    refactor: update styles and text

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,21 +1,49 @@
 import RegisterForm from '@/components/RegisterForm'
 import SignInForm from '@/components/SignInForm'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { useState } from 'react'
+import { Button } from 'react-day-picker'
 
 export default function SignIn() {
+  const [activeTab, setActiveTab] = useState<string>('sign-in')
+
   return (
-    <main className="container mx-auto bg-zinc-900/20 flex items-center justify-center w-screen h-screen">
-      <Tabs defaultValue="sign-in" className="w-full xl:w-1/4 md:w-2/5 sm:w-2/4 ">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="sign-in">Sign in</TabsTrigger>
-          <TabsTrigger value="register">Register</TabsTrigger>
-        </TabsList>
+    <main className="container mx-auto bg-background flex items-center justify-center w-screen h-screen">
+      <Tabs
+        onValueChange={setActiveTab}
+        value={activeTab}
+        defaultValue="sign-in"
+        className="w-full xl:w-1/4 md:w-2/5 sm:w-2/4  flex flex-col rounded-3xl border-[1.7px] border-secondary/50 bg-transparent shadow-3xl p-0 "
+      >
+        <p className="shadow-none bg-transparent text-3xl font-bold uppercase font-rubik p-6 pb-0 text-foreground/75">
+          {activeTab === 'sign-in' ? 'Sign in' : 'Register'}
+        </p>
+
         <TabsContent value="sign-in">
           <SignInForm />
         </TabsContent>
         <TabsContent value="register">
           <RegisterForm />
         </TabsContent>
+        <TabsList className="w-full bg-transparent shadow-none p-6 pt-0 flex justify-center gap-1">
+          <p className=" text-sm font-light text-foreground/90">
+            {activeTab === 'sign-in' ? "Don't have an account? " : 'Already have an account?'}
+          </p>
+          <TabsTrigger
+            value="register"
+            className={`shadow-none bg-transparent hidden text-sm font-normal text-primary p-0
+              ${activeTab === 'sign-in' ? 'flex' : ''}`}
+          >
+            Regsiter
+          </TabsTrigger>
+          <TabsTrigger
+            value="sign-in"
+            className={`shadow-none bg-transparent hidden text-sm font-normal text-primary p-0
+            ${activeTab !== 'sign-in' ? 'flex' : ''}`}
+          >
+            Sign in here
+          </TabsTrigger>
+        </TabsList>
       </Tabs>
     </main>
   )
