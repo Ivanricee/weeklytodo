0368b6d5f6cc72586755afa492ba860731112eb5
Iván Bautista <ivanbautistag@gmail.com>
2025-02-18 21:24:45 -0600

    feat: handle storage based on user type (guest or authenticated)

--- src/hook/useTaskActions.ts
+++ src/hook/useTaskActions.ts
@@ -1,7 +1,8 @@
 import { generateTaskId, readTaskFB, writeTaskFB } from '@/lib/firebase'
+import { guestUser, readLocalTask, writeLocalTask } from '@/lib/localStorage'
 import { initialWeekDays, type TaskByWeek } from '@/lib/utils'
 import { addTaskSchema } from '@/schemas/addtask-schema'
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useId, useRef, useState } from 'react'
 import { z } from 'zod'
 
 export type actionState = {
@@ -16,8 +17,19 @@ export default function useTaskActions() {
     loading: true,
     error: null,
   })
+
   const isInitialLoad = useRef(true)
   const readTaskCB = useCallback(async () => {
+    const localTask = await readLocalTask()
+    if (localTask) {
+      setWeekTasks(localTask)
+      if (isInitialLoad.current) {
+        setActionState((state) => ({ ...state, loading: false }))
+        isInitialLoad.current = false
+      }
+      return
+    }
+
     await readTaskFB((taskByWeek, error) => {
       setWeekTasks(taskByWeek)
       if (error) setActionState((state) => ({ ...state, error: error }))
@@ -32,9 +44,11 @@ export default function useTaskActions() {
   }, [])
 
   const writeTask = async (data: z.infer<typeof addTaskSchema>, day: number) => {
+    const localUser = guestUser() === 'Guest'
+    const localTaskId = `task-${Date.now()}-${Math.random().toString(36).substring(2, 9)}`
     setActionState((state) => ({ ...state, error: null }))
     const title = data.title
-    const draftTaskId = generateTaskId()
+    const draftTaskId = !localUser ? generateTaskId() : localTaskId
     if (draftTaskId) {
       const completed = false
       const date = Date.now()
@@ -42,13 +56,14 @@ export default function useTaskActions() {
       const newTask = { taskId: draftTaskId, title, date, day, completed, isLoading: true }
       copyWeekTasks[day].push(newTask)
       setWeekTasks(copyWeekTasks)
-      const { error } = await writeTaskFB(newTask)
+      const { error } = !localUser
+        ? await writeTaskFB(newTask)
+        : writeLocalTask(newTask, localTaskId)
       await readTaskCB()
       if (error) {
         setWeekTasks(structuredClone(weekTasks))
         return setActionState((state) => ({ ...state, error: error }))
       }
-      //return setActionState((cState) => ({ ...cState, loading: false }))
     }
   }
 
