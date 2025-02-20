e3b3bddf1fa8d9eb4784b61358cdad2697f3792b
Iván Bautista <ivanbautistag@gmail.com>
2025-02-19 18:46:26 -0600

    feat: add footer

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -5,6 +5,7 @@ import { Card, CardHeader } from '@/components/ui/card'
 import { useAuthContext } from '@/context/AuthContext'
 import { TaskProvider } from '@/context/TaskContext'
 import { getCurrentWeekText } from '@/lib/utils'
+import { Github, LinkedinIcon } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 export default function TodoList() {
@@ -23,7 +24,7 @@ export default function TodoList() {
   const userName = typeof user === 'string' ? user : user.email
   return (
     <main className="container mx-auto flex flex-col gap-4 w-screen h-screen max-w-[400px] animate-in fade-in-20 slide-in-from-top-4 px-7">
-      <header className="flex gap-2 justify-between w-full items-center py-4 h-24">
+      <header className="flex gap-2 justify-between w-full items-center py-4 h-20">
         <div>
           <Button variant={'outline'} onClick={logOut} disabled={authState.isLoading}>
             Log out
@@ -42,8 +43,65 @@ export default function TodoList() {
           <WeekTasksList />
         </TaskProvider>
       </Card>
-      <footer className="flex gap-2 justify-between w-full items-center py-4 h-24">
-        <h2>Footer</h2>
+      <footer className="flex flex-col items-center gap-4 text-sm text-secondary font-light pt-5">
+        <p className="text-center">
+          Educational practice project Inspired by{' '}
+          <a
+            href="https://weekstack.io/"
+            className="text-primary hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Weekstack
+          </a>
+        </p>
+
+        <div className="flex items-center gap-4 justify-center">
+          <Button
+            variant={'outline'}
+            asChild
+            className="p-0 transition-all duration-300 font-normal"
+          >
+            <Link
+              to="https://github.com/Ivanricee/weeklytodo"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-2"
+            >
+              <Github size={16} />
+              <span>Star</span>
+            </Link>
+          </Button>
+
+          <Link
+            to="https://www.linkedin.com/in/ivanrice/"
+            target="_blank"
+            className="group flex flex-nowrap gap-1 hover:text-foreground/80 transition-all duration-300"
+            rel="noopener noreferrer"
+          >
+            <LinkedinIcon size={16} />
+            <span>
+              <b className="text-secondary/90 group-hover:text-foreground/70 transition-all duration-300">
+                Ivanrice
+              </b>
+            </span>
+          </Link>
+
+          <span>|</span>
+
+          <Link
+            to="https://x.com/thebtjackson"
+            className="group hover:text-foreground/80 transition-all duration-300"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Design by{' '}
+            <b className="text-secondary/90 group-hover:text-foreground/70 transition-all duration-300">
+              {' '}
+              Brett
+            </b>
+          </Link>
+        </div>
       </footer>
     </main>
   )
